cc1e5ab200a2121150bc95ead06b94cd52eaaa4f
GAMR11 <gamr130898@gmail.com>
2026-01-26 22:58:41 -0500

    se seteo a false un test por verdadero para provocar error intencional

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,6 +11,6 @@ class ExampleTest extends TestCase
      */
     public function test_that_true_is_true(): void
     {
-        $this->assertTrue(true);
+        $this->assertTrue(false);//modificamos el test a false para que falle de manera intencional y evaluar el tiempo de respuesta luego del ajuste.
     }
 }
