32cfcccead962acc75dd5d08a1c18327a4b5bb25
Gustavo Morales <gmorales@tcc.com.ec>
2026-03-01 20:12:08 -0500

    update Jenkinsfile with stage DORA Metrics

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+      /**
+     * Deshabilitar CSRF para tests
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+    }
 }
