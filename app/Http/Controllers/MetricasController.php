455d78cd36ad065241fde357c5f4684ed9058213
Gustavo Morales <gmorales@tcc.com.ec>
2026-03-03 02:16:27 -0500

    KAN-1: Test jira integration

--- app/Http/Controllers/MetricasController.php
+++ app/Http/Controllers/MetricasController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\DB;
 
 class MetricasController extends Controller
 {
